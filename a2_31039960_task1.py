3e7d52c0f1c24673ebed98f465154822fa493f7f
Ashraf Bin Saleem <48388170+AshrafBinSaleem@users.noreply.github.com>
2020-05-29 08:46:26 +1000

    Finished reference matching and cleaning up task 1 code.

--- a2_31039960_task1.py
+++ a2_31039960_task1.py
@@ -24,57 +24,53 @@
 #   has omitted program comments, which are your responsibility to add. Any
 #   other 'placeholder' comments should be removed from your final submission.
 
+# creating a dictionary to store unique person name and store their person object.
+person_dict = {}
+
+
 class Person:
 
     def __init__(self, first_name, last_name):
         self.first_name = first_name
         self.last_name = last_name
         self.friends_list = []
-        #pass # placeholder only. To be implemented in task 1.
 
     def add_friend(self, friend_person):
         self.friends_list.append(friend_person)
-        #pass # placeholder only. To be implemented in task 1.
 
     def get_name(self):
         return self.first_name + " " + self.last_name
-        #pass # placeholder only. To be implemented in task 1.
 
     def get_friends(self):
         return self.friends_list
-        #pass # placeholder only. To be implemented in task 1.
+
+
+# creating a dictionary to store name and person object as friend
+def dict_loader(name):
+    if name not in person_dict:
+        person_first_name = name.split(" ")[0]
+        person_last_name = name.split(" ")[1]
+        person_dict[name] = Person(person_first_name, person_last_name)
+    return person_dict[name]
 
 
 def load_people():
     read_file = open("a2_sample_set.txt", "r")
     person_list = []
     for line in read_file:
-        #spliting each line
+        # splitting each line
         line_splited = line.split(": ")
 
-        #seperating first name and last name and seperating person from friend
-        person_first_name = line_splited[0].split(" ")[0]
-        person_last_name = line_splited[0].split(" ")[1]
-        person = Person(person_first_name, person_last_name)
+        person = dict_loader(line_splited[0])
 
-        #adding friends
+        # adding friends
         for friend_name in line_splited[1].split(", "):
-            friend_first_name = friend_name.split(" ")[0]
-            friend_last_name =  friend_name.split(" ")[1]
-            friend_person = Person(friend_first_name, friend_last_name)
+            friend_person = dict_loader(friend_name)
             person.add_friend(friend_person)
 
         person_list.append(person)
-        print(person.get_friends())
-    #pass # placeholder only. To be implemented in task 1.
-    print(person_list)
     return person_list
+
 
 if __name__ == '__main__':
     load_people()
-    #pass # placeholder only. You may add your own testing code within this
-         # main block to check if the code is working the way you expect.
-
-
-
-# do not add code here (outside the main block).
